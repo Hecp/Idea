93fec3c7ee20192f2d2690c676cc1abc5c7d489c
Hecp <hepingzhige13@163.com>
2016-03-17 15:45:30 +0800

    增加抽象工厂模式测试与UML类图

--- designpattern/src/main/java/com/hcp/designpattern/factory/abstractorfactory/Test.java
+++ designpattern/src/main/java/com/hcp/designpattern/factory/abstractorfactory/Test.java
@@ -6,9 +6,37 @@ package com.hcp.designpattern.factory.abstractorfactory;
  */
 public class Test {
     public static void main(String[] args) {
+        //创建高级汽车工厂
         CarFactory carFactory1 = new LuxuyCarFactory();
-        Engine engine = carFactory1.createEngine();
-        engine.start();
-        engine.run();
+
+        //创建高级汽车引擎
+        Engine engine1 = carFactory1.createEngine();
+        engine1.start();
+        engine1.run();
+
+        //创建高级汽车座椅
+        Seat seat1 = carFactory1.createSeat();
+        seat1.massage();;
+
+        //创建高级汽车轮胎
+        Tyre tyre1 = carFactory1.createTyre();
+        tyre1.revolve();
+
+        //创建低端汽车工厂
+        CarFactory carFactory2 = new LowCarFactory();
+
+        //创建低端汽车引擎
+        Engine engine2 = carFactory2.createEngine();
+        engine2.start();
+        engine2.run();
+
+        //创建低端汽车座椅
+        Seat seat2 = carFactory2.createSeat();
+        seat2.massage();
+
+        //创建低端汽车轮胎
+        Tyre tyre2 = carFactory2.createTyre();
+        tyre2.revolve();
+
     }
 }
