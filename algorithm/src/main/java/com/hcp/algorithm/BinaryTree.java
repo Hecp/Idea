7d2cd54f0db99ecff840189f9828a89445ffe8e0
Vincent <hepingzhige13@163.com>
2021-09-24 17:41:34 +0800

    add BinaryTree

---
+++ algorithm/src/main/java/com/hcp/algorithm/BinaryTree.java
@@ -0,0 +1,124 @@
+package com.hcp.algorithm;
+
+import java.util.*;
+
+public class BinaryTree {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    /**
+     * 从上到下打印二叉树
+     *
+     * 从上到下打印出二叉树的每个节点，同一层的节点按照从左到右的顺序打印。
+     *
+     * 例如:
+     * 给定二叉树: [3,9,20,null,null,15,7],
+     *
+     *     3
+     *    / \
+     *   9  20
+     *     /  \
+     *    15   7
+     * 返回：
+     *
+     * [3,9,20,15,7]
+     *
+     * @param
+     * @return 
+     * @author hcp
+     * @date 2021/9/24 16:00
+     */ 
+    public static int[] levelOrder(TreeNode root) {
+        if (root == null) {
+            return new int[0];
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        List<Integer> list = new ArrayList<>(queue.size());
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            list.add(node.val);
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+        int[] res = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+        return res;
+    }
+
+    /**
+     * 从上到下打印二叉树 II
+     *
+     * 从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，每一层打印到一行。
+     *
+     *  
+     *
+     * 例如:
+     * 给定二叉树: [3,9,20,null,null,15,7],
+     *
+     *     3
+     *    / \
+     *   9  20
+     *     /  \
+     *    15   7
+     * 返回其层次遍历结果：
+     *
+     * [
+     *   [3],
+     *   [9,20],
+     *   [15,7]
+     * ]
+     *
+     * @param
+     * @return
+     * @author hcp
+     * @date 2021/9/24 16:39
+     */
+    public static List<List<Integer>> levelOrderByLevel(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>(0);
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        List<List<Integer>> list = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            List<Integer> nodeList = new ArrayList<>(2);
+            for (int i = queue.size(); i > 0 ; i--) {
+                TreeNode node = queue.poll();
+                nodeList.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            list.add(nodeList);
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        TreeNode right = new TreeNode(20);
+        root.right = right;
+        right.left = new TreeNode(15);
+        right.right = new TreeNode(7);
+//        System.out.println(Arrays.toString(levelOrder(root)));
+        System.out.println((levelOrderByLevel(root)));
+    }
+}
--- algorithm/src/main/java/com/hcp/algorithm/FindAlgorithm.java
+++ algorithm/src/main/java/com/hcp/algorithm/FindAlgorithm.java
@@ -10,16 +10,19 @@ public class FindAlgorithm {
 //        System.out.println(findRepeatNumber(new int[]{2,3,5,3,2}));
 //        System.out.println(searchTimesByMap(new int[]{5,7,7,8,8,10}, 7));
 //        System.out.println(searchTimesByBinarySearch(new int[]{5,7,7,8,8,10}, 7));
-        System.out.println(missingNumber(new int[]{0}));
+//        System.out.println(missingNumber(new int[]{0}));
+//        int[][] matrix = {{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}};
+//        System.out.println(findNumberIn2DArray(matrix, 2));
+//        System.out.println(minArray(new int[]{3,4,5,1,2}));
+        System.out.println(firstUniqChar("abaccdeff"));
     }
 
     /**
-     *
      * 找出数组中重复的数字。
-     *
+     * <p>
      * 在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，
      * 也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
-
+     * <p>
      * 示例 1：
      * 输入：
      * [2, 3, 1, 0, 2, 5, 3]
@@ -47,9 +50,9 @@ public class FindAlgorithm {
 
     /**
      * 统计一个数字在排序数组中出现的次数。
-     *
+     * <p>
      * 示例 1:
-     *
+     * <p>
      * 输入: nums = [5,7,7,8,8,10], target = 8
      * 输出: 2
      *
@@ -76,9 +79,9 @@ public class FindAlgorithm {
 
     /**
      * 统计一个数字在排序数组中出现的次数。
-     *
+     * <p>
      * 示例 1:
-     *
+     * <p>
      * 输入: nums = [5,7,7,8,8,10], target = 8
      * 输出: 2
      *
@@ -121,12 +124,12 @@ public class FindAlgorithm {
 
     /**
      * 有序数组自然想到二分查找
-     *
+     * <p>
      * 一个长度为n-1的递增排序数组中的所有数字都是唯一的，并且每个数字都在范围0～n-1之内。
      * 在范围0～n-1内的n个数字中有且只有一个数字不在该数组中，请找出这个数字。
-     *
+     * <p>
      * 示例 1:
-     *
+     * <p>
      * 输入: [0,1,3]
      * 输出: 2
      *
@@ -146,5 +149,108 @@ public class FindAlgorithm {
             }
         }
         return left;
+    }
+
+    /**
+     * 二维数组中的查找
+     * 在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+     * 请完成一个高效的函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+     *
+     * 示例:
+     *
+     * 现有矩阵 matrix 如下：
+     *
+     * [
+     *   [1,   4,  7, 11, 15],
+     *   [2,   5,  8, 12, 19],
+     *   [3,   6,  9, 16, 22],
+     *   [10, 13, 14, 17, 24],
+     *   [18, 21, 23, 26, 30]
+     * ]
+     * 给定 target = 5，返回 true。
+     *
+     * 给定 target = 20，返回 false。
+     *
+     * @param
+     * @return 
+     * @author hcp
+     * @date 2021/9/24 13:53
+     */ 
+    public static boolean findNumberIn2DArray(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int rows = matrix.length, columns = matrix[0].length;
+        int row = 0, column = columns - 1;
+        while (row < rows && column >= 0) {
+            int num = matrix[row][column];
+            if (num == target) {
+                return true;
+            } else if (num > target) {
+                column--;
+            } else {
+                row++;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 旋转数组的最小数字
+     *
+     * 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。
+     * 例如，数组 [3,4,5,1,2] 为 [1,2,3,4,5] 的一个旋转，该数组的最小值为1。  
+     *
+     * 示例 1：
+     *
+     * 输入：[3,4,5,1,2]
+     * 输出：1
+     *
+     * @param
+     * @return 
+     * @author hcp
+     * @date 2021/9/24 14:11
+     */ 
+    public static int minArray(int[] numbers) {
+        int left = 0, right = numbers.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (numbers[mid] > numbers[right]) {
+                left = mid + 1;
+            } else if (numbers[mid] < numbers[right]) {
+                right = mid;
+            } else {
+                right--;
+            }
+        }
+        return numbers[left];
+    }
+
+    /**
+     * 第一个只出现一次的字符
+     * 在字符串 s 中找出第一个只出现一次的字符。如果没有，返回一个单空格。 s 只包含小写字母。
+     *
+     * 示例 1:
+     *
+     * 输入：s = "abaccdeff"
+     * 输出：'b'
+     *
+     * @param
+     * @return 
+     * @author hcp
+     * @date 2021/9/24 15:02
+     */ 
+    public static char firstUniqChar(String s) {
+        Map<Character, Integer> map = new HashMap<Character, Integer>(s.length());
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) == 1) {
+                return s.charAt(i);
+            }
+        }
+        return ' ';
     }
 }
