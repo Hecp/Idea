f316651d4cb6cd5e5994517841e6abaa32738927
Vincent <hepingzhige13@163.com>
2021-09-26 16:39:31 +0800

    add search back tracking

--- algorithm/src/main/java/com/hcp/algorithm/BinaryTree.java
+++ algorithm/src/main/java/com/hcp/algorithm/BinaryTree.java
@@ -200,29 +200,131 @@ public class BinaryTree {
      * @date 2021/9/25 20:39
      */
     public static boolean isSubStructure(TreeNode A, TreeNode B) {
-        return ((A != null && B != null) && (recur(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B)));
+        return (A != null && B != null) && (hashSubStructure(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B));
 
     }
 
-    private static boolean recur(TreeNode a, TreeNode b) {
+    private static boolean hashSubStructure(TreeNode a, TreeNode b) {
         if (b == null) {
             return true;
         }
         if (a == null || a.val != b.val) {
             return false;
         }
-        return recur(a.left, b.left) && recur(a.right, b.right);
+        return hashSubStructure(a.left, b.left) && hashSubStructure(a.right, b.right);
+    }
+
+    /**
+     * 二叉树的镜像
+     *
+     * 请完成一个函数，输入一个二叉树，该函数输出它的镜像。
+     *
+     * 例如输入：
+     *
+     *      4
+     *    /   \
+     *   2     7
+     *  / \   / \
+     * 1   3 6   9
+     * 镜像输出：
+     *
+     *      4
+     *    /   \
+     *   7     2
+     *  / \   / \
+     * 9   6 3   1
+     *
+     *  
+     *
+     * 示例 1：
+     *
+     * 输入：root = [4,2,7,1,3,6,9]
+     * 输出：[4,7,2,9,6,3,1]
+     *
+     * @author hcp
+     * @date 2021/9/26 11:12
+     */
+    public static TreeNode mirrorTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = mirrorTree(root.left);
+        TreeNode right = mirrorTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+
+    /**
+     * 对称的二叉树
+     *
+     * 请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
+     *
+     * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+     *
+     *     1
+     *    / \
+     *   2   2
+     *  / \ / \
+     * 3  4 4  3
+     * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+     *
+     *     1
+     *    / \
+     *   2   2
+     *    \   \
+     *    3    3
+     *
+     *  
+     *
+     * 示例 1：
+     *
+     * 输入：root = [1,2,2,3,4,4,3]
+     * 输出：true
+     * 示例 2：
+     *
+     * 输入：root = [1,2,2,null,3,null,3]
+     * 输出：false
+     *
+     * @author hcp
+     * @date 2021/9/26 16:22
+     */
+    public static boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return childIsSymmetric(root.left, root.right);
+    }
+
+    private static boolean childIsSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null || left.val != right.val) {
+            return false;
+        }
+        return childIsSymmetric(left.left, right.right) && childIsSymmetric(left.right, right.left);
     }
 
     public static void main(String[] args) {
         TreeNode root = new TreeNode(3);
-        root.left = new TreeNode(9);
-        TreeNode right = new TreeNode(20);
+        TreeNode left = new TreeNode(9);
+        root.left = left;
+        left.left  = new TreeNode(7);
+        left.right  = new TreeNode(15);
+        TreeNode right = new TreeNode(9);
         root.right = right;
         right.left = new TreeNode(15);
         right.right = new TreeNode(7);
+
 //        System.out.println(Arrays.toString(levelOrder(root)));
 //        System.out.println((levelOrderByLevel(root)));
-        System.out.println((levelOrderByLevel2(root)));
+//        System.out.println((levelOrderByLevel2(root)));
+        TreeNode childTree = new TreeNode(20);
+        childTree.left = new TreeNode(15);
+//        System.out.println(isSubStructure(root, childTree));
+//        mirrorTree(root);
+        System.out.println(isSymmetric(root));
+
     }
 }
