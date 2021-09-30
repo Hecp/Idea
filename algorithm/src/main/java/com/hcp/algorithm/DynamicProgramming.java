0dfef06749ef1ee43f6789e102ae2d051ec337e7
Vincent <hepingzhige13@163.com>
2021-09-30 09:41:26 +0800

    add translate number

--- algorithm/src/main/java/com/hcp/algorithm/DynamicProgramming.java
+++ algorithm/src/main/java/com/hcp/algorithm/DynamicProgramming.java
@@ -169,6 +169,40 @@ public class DynamicProgramming {
         return grid[rows - 1][columns - 1];
     }
 
+    /**
+     * 把数字翻译成字符串
+     *
+     * 给定一个数字，我们按照如下规则把它翻译为字符串：0 翻译成 “a” ，1 翻译成 “b”，……，11 翻译成 “l”，……，25 翻译成 “z”。
+     * 一个数字可能有多个翻译。请编程实现一个函数，用来计算一个数字有多少种不同的翻译方法。
+     *
+     * 示例 1:
+     *
+     * 输入: 12258
+     * 输出: 5
+     * 解释: 12258有5种不同的翻译，分别是"bccfi", "bwfi", "bczi", "mcfi"和"mzi"
+     *
+     * @author hcp
+     * @date 2021/9/28 19:06
+     */
+    public static int translateNum(int num) {
+        String s = String.valueOf(num);
+        int p = 0, q = 0, r = 1;
+        for (int i = 0; i < s.length(); i++) {
+            p = q;
+            q = r;
+            r = 0;
+            r += q;
+            if (i == 0) {
+                continue;
+            }
+            String pre = s.substring(i - 1, i + 1);
+            if (pre.compareTo("25") <= 0 && pre.compareTo("10") >= 0) {
+                r += p;
+            }
+        }
+        return r;
+    }
+
     public static void main(String[] args) {
 //        System.out.println(fib(8));
 //        System.out.println(numWays(8));
@@ -178,6 +212,7 @@ public class DynamicProgramming {
                 {1,3,1},
                 {1,5,1},
                 {4,2,1}};
-        System.out.println(maxValue(matrix));
+//        System.out.println(maxValue(matrix));
+        System.out.println(translateNum(12258));
     }
 }
